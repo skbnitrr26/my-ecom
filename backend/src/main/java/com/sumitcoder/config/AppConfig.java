121f9dfd9e9f267699719a52ccc2eaee86b4ef74
Sumit Kumar <skbkdh2017@gmail.com>
2025-10-18 16:52:37 +0530

    Update config file

--- backend/src/main/java/com/sumitcoder/config/AppConfig.java
+++ backend/src/main/java/com/sumitcoder/config/AppConfig.java
@@ -28,20 +28,18 @@ public class AppConfig {
 
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(Authorize -> Authorize
-//                		.requestMatchers("/api/admin/**").hasAnyRole("SHOP_OWNER","ADMIN")
-                                .requestMatchers("/api/**").authenticated()
-                                .requestMatchers("/api/products/*/reviews").permitAll()
-                                .anyRequest().permitAll()
-                )
+                        // .requestMatchers("/api/admin/**").hasAnyRole("SHOP_OWNER","ADMIN")
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/api/products/*/reviews").permitAll()
+                        .anyRequest().permitAll())
                 .addFilterBefore(new JwtTokenValidator(), BasicAuthenticationFilter.class)
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
-               
-		
-		return http.build();
-		
-	}
-	
+
+        return http.build();
+
+    }
+
     // CORS Configuration
     private CorsConfigurationSource corsConfigurationSource() {
         return new CorsConfigurationSource() {
@@ -49,7 +47,7 @@ public class AppConfig {
             public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                 CorsConfiguration cfg = new CorsConfiguration();
                 cfg.setAllowedOrigins(Arrays.asList(
-//                        "https://zosh-bazzar-zosh.vercel.app",
+                        "https://sam-kart.netlify.app",
                         "http://localhost:3000",
                         "http://localhost:5173"));
                 cfg.setAllowedMethods(Collections.singletonList("*"));
@@ -64,8 +62,8 @@ public class AppConfig {
 
     @Bean
     PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public RestTemplate restTemplate() {
